271e2be4a8b62595bbec159857b73ff12825d0e6
AyvR12 <101042558+AyvR12@users.noreply.github.com>
2022-08-20 17:30:51 +0400

    toDoList

---
+++ script.js
@@ -0,0 +1,23 @@
+let textInputic = [];
+const input = document.querySelector('#input');
+
+function stanalArjeq(arj){
+    textInputic = arj;
+
+}
+
+function add(){
+    let li = document.createElement('li');
+    li.innerText = textInputic;
+     input.value = "";
+    let ul = document.querySelector('ul');
+    ul.appendChild(li);
+    
+}
+
+
+function del(){
+    let list  = document.getElementById('tasks');
+    list.removeChild(list.childNodes[0]);
+
+}
